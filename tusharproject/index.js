ec25174f19c235b3bc9cdefe127c594f72bdfa35
Pankaj20202024 <121535589+Pankaj20202024@users.noreply.github.com>
2023-10-08 02:19:28 +0530

    Add files via upload

---
+++ tusharproject/index.js
@@ -0,0 +1,12 @@
+
+
+function scroll_left() {
+
+    var left1 = document.querySelector("#slider");
+    left1.scrollBy(-350, 0)
+}
+
+function scroll_right() {
+    var right = document.querySelector("#slider");
+    right.scrollBy(350, 0)
+}
